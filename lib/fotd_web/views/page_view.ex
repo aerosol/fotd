556f38093a58a459be148b459eb7fbaaaf060197
Adam Rutkowski <hq@mtod.org>
2018-11-07 14:58:28 +0100

    Add "Tweet it" link

--- lib/fotd_web/views/page_view.ex
+++ lib/fotd_web/views/page_view.ex
@@ -15,12 +15,30 @@ defmodule FotdWeb.PageView do
     |> raw()
   end
 
-  def hexdocs_link(%{app: app, module: m, func: f, arity: a} = fotd) do
+  def hexdocs_link(fotd) do
     # TODO see if we need any escaping here for non alphanumeric names etc.
-    link to: "https://hexdocs.pm/#{app}/#{inspect(m)}.html##{f}/#{a}" do
+    link to: make_hexdocs_url(fotd) do
       mfa(fotd)
     end
   end
 
+  def make_hexdocs_url(%{app: app, module: m, func: f, arity: a}) do
+    "https://hexdocs.pm/#{app}/#{inspect(m)}.html##{f}/#{a}"
+  end
+
   def mfa(%{module: m, func: f, arity: a}), do: "#{inspect(m)}.#{f}/#{a}"
+
+  def tweet_link(fotd, do: content) do
+    base = "https://twitter.com/intent/tweet"
+
+    twitter_params = %{
+      original_referer: "https://elixirfunctionoftheday.com",
+      ref_src: "twsrc^tfw",
+      text: "Look what I found! #{mfa(fotd)} #{make_hexdocs_url(fotd)} https://elixirfunctionoftheday.com #elixir"
+    }
+
+    link to: "#{base}?#{URI.encode_query(twitter_params)}" do
+      raw content
+    end
+  end
 end
